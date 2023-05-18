0d75ea5937b99d5787c1c68c14d9184985df8db0
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-05-18 13:03:34 +0530

    updated

---
+++ PLA Java/pattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class pattern{
+    public static void main(String args[]){
+        for(int i=1; i<=5; i++){
+            System.out.println();
+            for(int j = 1; i<=5; j++){
+                System.out.print(j);
+                if(i==j){
+                    break;
+                }
+            }
+        }
+        for(int i=1; i<=5; i++){
+            System.out.println();
+            for(int j = 5; j>0; j--){
+                System.out.print(6 - j);
+                if(i==j){
+                    break;
+                }
+            }
+        }
+    }
+}
