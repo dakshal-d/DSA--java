c8f9181fe8c4fa9f6c74794eaed969239f5e9da1
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-06-12 01:05:50 +0530

    Updated

--- LinkedList/DLL1.java
+++ LinkedList/DLL1.java
@@ -1,31 +1,7 @@
 package LinkedList;
+
+// You are using Java
 import java.util.Scanner;
-
-public class DLL1 {
-    public static void main(String[] args) {
-        DLL d = new DLL();
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            int x = sc.nextInt();
-            if (x == 1) {
-                int a = sc.nextInt();
-                d.insertfirst(a);
-            } else if (x == 2) {
-                int a = sc.nextInt();
-                d.insertlast(a);
-            } else if (x == 3) {
-                d.traverse();
-            }
-            else if (x == 4) {
-                int a = sc.nextInt();
-                int n = sc.nextInt();
-                d.insertAtPosition(a, n);
-            }
-
-        }
-    }
-}
 
 class Node {
     int data;
@@ -45,75 +21,113 @@ class DLL {
 
     DLL() {
         head = null;
+        tail = null;
     }
 
-    public void insertfirst(int data) {
+    public void insertFront(int data) {
         Node node = new Node(data);
         node.next = head;
         node.prev = null;
         if (head != null) {
             head.prev = node;
+            head = node;
         }
+        if (head == null) {
         head = node;
+        tail = node;
+        }
+        
+        System.out.println("Node with data " + node.data + " inserted at the front");
     }
 
-    public void insertlast(int data) {
-        Node node = new Node(data);
-        Node last = head;
-
-        node.next = null;
-
+    public void insertEnd(int data) {
+        Node newNode = new Node(data);
         if (head == null) {
-            node.prev = null;
-            head = node;
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        System.out.println("Node with data " + data + " inserted at the end.");
+    }
+
+    public void insertPos(int data, int position) {
+        if (position <= 0) {
+            System.out.println("Invalid position.");
             return;
         }
-
-        while (last.next != null) {
-            last = last.next;
+        Node newNode = new Node(data);
+        if (position == 1) {
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+        } else {
+            Node curr = head;
+            int count = 1;
+            while (count < position - 1 && curr != null) {
+                curr = curr.next;
+                count++;
+            }
+            if (curr == null) {
+                System.out.println("Invalid position.");
+                return;
+            }
+            newNode.prev = curr;
+            newNode.next = curr.next;
+            if (curr.next != null) {
+                curr.next.prev = newNode;
+            } else {
+                tail = newNode;
+            }
+            curr.next = newNode;
         }
 
-        last.next = node;
-        node.prev = last;
+        System.out.println("Node with data " + data + " inserted at position " + position + ".");
     }
 
     public void traverse() {
         Node node = head;
+        System.out.print("Doubly Linked List: ");
         while (node != null) {
             System.out.print(node.data + " ");
             node = node.next;
         }
-
+        System.out.println();
     }
+}
 
-    public void insertAtPosition(int data, int position) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
+public class DLL1 {
+    public static void main(String[] args) {
+        DLL d = new DLL();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int x = sc.nextInt();
+            if (x == 1) {
+                int q = sc.nextInt();
+                d.insertFront(q);
+            } else if (x == 2) {
+                int q = sc.nextInt();
+                d.insertEnd(q);
+            } else if (x == 3) {
+                int q = sc.nextInt();
+                int n = sc.nextInt();
+                d.insertPos(q, n);
+            } else if (x == 4) {
+                d.traverse();
+            } else if (x == 5) {
+                break;
+            } else {
+
+                System.out.println("Invalid choice. Please try again.");
+            }
+
         }
-        if (position == 1) {
-            insertfirst(data);
-            return;
-        }
-        Node current = head;
-        int count = 1;
-        while (count < position - 1 && current.next != null) {
-            current = current.next;
-            count++;
-        }
-
-        if (current.next == null) {
-            current.next = newNode;
-            newNode.prev = current;
-        } else {
-            newNode.next = current.next;
-            newNode.prev = current;
-            current.next.prev = newNode;
-            current.next = newNode;
-        }
-
-
-
-}}
-
+    }
+}
