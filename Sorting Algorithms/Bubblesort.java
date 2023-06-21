efde47ac3628ad8c1d11754a2209f598b93702ee
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-06-21 20:49:34 +0530

    Modified Bubble sort

--- Sorting Algorithms/Bubblesort.java
+++ Sorting Algorithms/Bubblesort.java
@@ -2,22 +2,29 @@ import java.util.Arrays;
 
 public class Bubblesort {
     public static void main(String[] args) {
-        int[] arr={3,4,5,1,2};
+        int[] arr={1,2,3,4,5};
         bubblest(arr);
         System.out.print(Arrays.toString(arr));
         
     }
     static void bubblest(int[] arr){
+        boolean swap;
         for(int i=0; i<arr.length;i++){
+            swap=false;
             for(int j=1;j<arr.length-i;j++){
                 if (arr[j-1]>arr[j]){
                     int temp=arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
+                    swap=true;
                 }
 
             }
+            if(!swap){
+                break;
+            }
         }
+    
     }
     
 }
