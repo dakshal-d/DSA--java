0ef0a930e459a40c30dea14f4e58b598486812cf
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-06-22 02:49:42 +0530

    CS

---
+++ Sorting Algorithms/CS.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class CS {
+    public static void main(String[] args) {
+        int[] arr={2,5,4,3,1};
+        cycle(arr);
+        System.out.print(Arrays.toString(arr));
+        
+    }
+    public static void cycle(int[] arr){
+        int i=0;
+        while (i<arr.length){
+            int correct= arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }else{
+                i++;
+            }
+        }
+    }
+}
