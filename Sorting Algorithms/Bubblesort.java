525ffa28014de2679c02509f18a338509326f555
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-06-21 20:41:20 +0530

    BubbleSort

---
+++ Sorting Algorithms/Bubblesort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Bubblesort {
+    public static void main(String[] args) {
+        int[] arr={3,4,5,1,2};
+        bubblest(arr);
+        System.out.print(Arrays.toString(arr));
+        
+    }
+    static void bubblest(int[] arr){
+        for(int i=0; i<arr.length;i++){
+            for(int j=1;j<arr.length-i;j++){
+                if (arr[j-1]>arr[j]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+
+            }
+        }
+    }
+    
+}
