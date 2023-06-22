b48fd3ca1bec697a92b0abdeae626af20eed6c4c
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-06-22 21:01:38 +0530

    Updated patterns

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -6,7 +6,7 @@ public class Pattern1{
 
     static void pattern2(int n) {
         int originalN = n;
-        n = 2 * n-1;
+        n = 2 * n-2;
         for (int row = 0; row <= n; row++) {
             for (int col = 0; col <= n; col++) {
                 int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
