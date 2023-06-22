7665d5a6ab195a1e499d9bc80ff09984e43136b8
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-06-22 21:00:08 +0530

    Patterns

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,33 @@
+public class Pattern1{
+    public static void main(String[] args) {
+        pat1(5);
+        pattern2(4);
+    }
+
+    static void pattern2(int n) {
+        int originalN = n;
+        n = 2 * n-1;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+    static void pat1(int size){
+    for (int i = 0; i < size; i++) {
+      // print spaces
+      for (int j = 0; j < size - i; j++) {
+        System.out.print("  ");
+      }
+      // print stars
+      for (int k = 0; k <= i; k++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+    // loop to print the patt
+        
+}
