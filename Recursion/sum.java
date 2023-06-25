5f88335b169ab3732947459d237b2c2931587efb
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-06-25 17:37:08 +0530

    Recursion

---
+++ Recursion/sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        System.out.println(sum1(n));
+        
+    }
+    static int sum1(int n){
+        if (n==0 || n == 1 ){
+            return n ;
+        }else{
+            return n+sum1(n-1);
+    }
+    
+}
+}
---
+++ Sieve.java
@@ -0,0 +1,9 @@
+public class Sieve {
+    public static void main(String[] args) {
+        
+    }
+    static void prime(int n){
+        
+    }
+    
+}
