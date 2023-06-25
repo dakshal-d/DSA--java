c8d2c2b34387304500b2482983a264a069402bf9
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-06-25 17:43:13 +0530

    Updated

---
+++ Recursion/prodofDigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class prodofDigits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        System.out.println(prod(n));
+    }
+    static int prod(int n){
+        if(n%10==n){
+            return n;
+        }else{
+            return (n%10)*prod(n/10);
+        }
+    }
+}
